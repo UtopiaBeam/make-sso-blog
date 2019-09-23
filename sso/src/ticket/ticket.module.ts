d1960acf102150bfa237e5469942fcf267f0e8c2
UtopiaBeam <utopiabeam@gmail.com>
2019-09-23 13:13:30 +0700

    :fix: Fix missing dependencies

--- sso/src/ticket/ticket.module.ts
+++ sso/src/ticket/ticket.module.ts
@@ -3,12 +3,16 @@ import { TicketController } from './ticket.controller';
 import { TicketService } from './ticket.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TicketModel, TicketSchema } from './ticket.schema';
+import { ClientModule } from '../client/client.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: TicketModel, schema: TicketSchema },
         ]),
+        ClientModule,
+        UserModule,
     ],
     controllers: [TicketController],
     providers: [TicketService],
